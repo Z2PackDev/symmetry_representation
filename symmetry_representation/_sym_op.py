8ea3519e22858ae2432222ba9d43b1fda5b4e83f
Dominik Gresch <greschd@gmx.ch>
2018-08-17 16:20:13 +0200

    Add tools to check the order of a symmetry.

--- symmetry_representation/_sym_op.py
+++ symmetry_representation/_sym_op.py
@@ -90,6 +90,33 @@ class SymmetryOperation(SimpleHDF5Mapping, types.SimpleNamespace):
             and self.repr == other.repr
         )
 
+    def __matmul__(self, other):
+        if not isinstance(other, SymmetryOperation):
+            raise TypeError(
+                'Cannot matrix-multiply objects of type {} and {}'.format(
+                    type(self), type(other)
+                )
+            )
+        new_real_space_op = self.real_space_operator @ other.real_space_operator
+        new_repr = self.repr @ other.repr
+        return SymmetryOperation(
+            rotation_matrix=new_real_space_op.rotation_matrix,
+            translation_vector=new_real_space_op.translation_vector,
+            repr_matrix=new_repr.matrix,
+            repr_has_cc=new_repr.has_cc
+        )
+
+    def get_order(self, max_order=20):
+        curr_val = self
+        for i in range(1, max_order + 1):
+            if curr_val.repr.is_identity and curr_val.real_space_operator.is_lattice_translation:
+                return i
+            curr_val @= self
+        else:
+            raise ValueError(
+                'Order of the symmetry operation could not be determined.'
+            )
+
     @classmethod
     def from_hdf5(cls, hdf5_handle):
         if 'real_space_operator' in hdf5_handle:
@@ -127,14 +154,37 @@ class RealSpaceOperator(SimpleHDF5Mapping, types.SimpleNamespace):
             translation_vector = np.zeros(len(self.rotation_matrix))
         self.translation_vector = translation_vector
 
-    def __matmul__(self, r):
-        return self.apply(r)
+    def __matmul__(self, other):
+        if not isinstance(other, RealSpaceOperator):
+            raise TypeError(
+                'Cannot matrix-multiply objects of type {} and {}'.format(
+                    type(self), type(other)
+                )
+            )
+        return RealSpaceOperator(
+            rotation_matrix=self.rotation_matrix @ other.rotation_matrix,
+            translation_vector=self.translation_vector +
+            self.rotation_matrix @ other.translation_vector
+        )
 
     def apply(self, r):
         """
         Apply symmetry operation to a vector in reduced real-space coordinates.
         """
         return self.rotation_matrix @ r + self.translation_vector
+
+    @property
+    def is_pure_translation(self):
+        n, m = self.rotation_matrix.shape
+        if n != m:
+            return False
+        return np.allclose(self.rotation_matrix, np.eye(n))
+
+    @property
+    def is_lattice_translation(self):
+        return self.is_pure_translation and np.allclose(
+            self.translation_vector, np.round(self.translation_vector)
+        )
 
     def __eq__(self, other):
         return np.all(
@@ -153,6 +203,27 @@ class Representation(SimpleHDF5Mapping, types.SimpleNamespace):
     def __init__(self, matrix, has_cc=False):
         self.matrix = matrix
         self.has_cc = has_cc
+
+    def __matmul__(self, other):
+        if not isinstance(other, Representation):
+            raise TypeError(
+                'Cannot matrix-multiply objects of type {} and {}'.format(
+                    type(self), type(other)
+                )
+            )
+        if self.has_cc:
+            new_mat = self.matrix @ other.matrix.conjugate()
+        else:
+            new_mat = self.matrix @ other.matrix
+        new_has_cc = self.has_cc != other.has_cc
+        return Representation(matrix=new_mat, has_cc=new_has_cc)
+
+    @property
+    def is_identity(self):
+        n, m = self.matrix.shape
+        if n != m:
+            return False
+        return (not self.has_cc) and np.allclose(self.matrix, np.eye(n))
 
     def __eq__(self, val):
         return np.all(self.matrix == val.matrix) and self.has_cc == val.has_cc
