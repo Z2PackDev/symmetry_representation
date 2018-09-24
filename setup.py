b2eae55e2a474c4ab610c1f5d1cb8ef499e42d55
Dominik Gresch <greschd@gmx.ch>
2018-09-24 11:59:12 +0200

    Add test for Python 3.7.

--- setup.py
+++ setup.py
@@ -41,6 +41,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Intended Audience :: Science/Research',
         'Topic :: Scientific/Engineering :: Physics',
         'Development Status :: 4 - Beta'
