3001a307a8c9bc29c394a8c71d839c2755ab4257
Dominik Gresch <greschd@gmx.ch>
2018-09-24 12:24:00 +0200

    Add test for automatic representation matrix from example.

---
+++ tests/test_auto_repr.py
@@ -0,0 +1,64 @@
+"""
+Test that the automatically generated symmetry representations match a reference.
+"""
+
+import numpy as np
+import pymatgen as mg
+import pymatgen.symmetry.analyzer  # pylint: disable=unused-import
+import symmetry_representation as sr
+
+
+def test_auto_repr(sample):
+    """
+    Test that the symmetry group created with the automatic representation matrix
+    is the matches a reference.
+    """
+    pos_In = (0, 0, 0)  # pylint: disable=invalid-name
+    pos_As = (0.25, 0.25, 0.25)  # pylint: disable=invalid-name
+
+    orbitals = []
+    for spin in (sr.SPIN_UP, sr.SPIN_DOWN):
+        orbitals.extend([
+            sr.Orbital(position=pos_In, function_string=fct, spin=spin)
+            for fct in sr.WANNIER_ORBITALS['s'] + sr.WANNIER_ORBITALS['p']
+        ])
+        orbitals.extend([
+            sr.Orbital(position=pos_As, function_string=fct, spin=spin)
+            for fct in sr.WANNIER_ORBITALS['p']
+        ])
+
+    structure = mg.Structure(
+        lattice=[[0., 3.029, 3.029], [3.029, 0., 3.029], [3.029, 3.029, 0.]],
+        species=['In', 'As'],
+        coords=np.array([pos_In, pos_As])
+    )
+
+    analyzer = mg.symmetry.analyzer.SpacegroupAnalyzer(structure)
+    symops = analyzer.get_symmetry_operations(cartesian=False)
+    symops_cart = analyzer.get_symmetry_operations(cartesian=True)
+
+    symmetry_group = sr.SymmetryGroup(
+        symmetries=[
+            sr.SymmetryOperation.from_orbitals(
+                orbitals=orbitals,
+                real_space_operator=sr.RealSpaceOperator.
+                from_pymatgen(sym_reduced),
+                rotation_matrix_cartesian=sym_cart.rotation_matrix,
+                numeric=True
+            ) for sym_reduced, sym_cart in zip(symops, symops_cart)
+        ],
+        full_group=True
+    )
+    reference = sr.io.load(sample('symmetries_InAs.hdf5'))
+    assert symmetry_group.full_group == reference.full_group
+    for sym1, sym2 in zip(symmetry_group.symmetries, reference.symmetries):
+        assert sym1.repr.has_cc == sym2.repr.has_cc
+        assert np.allclose(sym1.repr.matrix, sym2.repr.matrix)
+        assert np.allclose(
+            sym1.real_space_operator.rotation_matrix,
+            sym2.real_space_operator.rotation_matrix
+        )
+        assert np.allclose(
+            sym1.real_space_operator.translation_vector,
+            sym2.real_space_operator.translation_vector
+        )
